ca2fcaaa16886de7b0905d91009ebb664e4b23ee
tgm-tmy <165565019+tgm-tmy@users.noreply.github.com>
2024-04-18 15:06:27 +0900

    switch router from gorilla to gin

--- api/router.go
+++ api/router.go
@@ -2,27 +2,27 @@ package api
 
 import (
 	"database/sql"
-	"net/http"
 
-	"github.com/gorilla/mux"
-
+	"github.com/gin-gonic/gin"
 	"github.com/tgm-tmy/go-api/api/middlewares"
 	"github.com/tgm-tmy/go-api/controllers"
 	"github.com/tgm-tmy/go-api/services"
 )
 
-func NewRouter(db *sql.DB) *mux.Router {
+func NewRouter(db *sql.DB) *gin.Engine {
 	ser := services.NewMyAppService(db)
 	aCon := controllers.NewArticleController(ser)
 	cCon := controllers.NewCommentController(ser)
-	r := mux.NewRouter()
-	r.HandleFunc("/article", aCon.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", aCon.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", aCon.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
 
-	r.Use(middlewares.LoggingMiddleware)
+	r := gin.Default()
+
+	r.POST("/article", aCon.PostArticleHandler)
+	r.GET("/article/list", aCon.ArticleListHandler)
+	r.GET("/article/:id", aCon.ArticleDetailHandler)
+	r.POST("/article/nice", aCon.PostNiceHandler)
+	r.POST("/comment", cCon.PostCommentHandler)
+
+	r.Use(middlewares.LoggingMiddleware())
 
 	return r
 }
